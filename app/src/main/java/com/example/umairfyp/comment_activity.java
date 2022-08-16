09a5ec9819115256f849abfea3a6405c3922d606
Umair <umairakram572@gmail.com>
2022-08-16 14:53:40 +0500

    work left
    score sharing
    players img , team img
    comment and news

---
+++ app/src/main/java/com/example/umairfyp/comment_activity.java
@@ -0,0 +1,14 @@
+package com.example.umairfyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class comment_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
