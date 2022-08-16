366af87bcd32361ca8f4dba18024db08884a4159
Umair <umairakram572@gmail.com>
2022-08-16 18:17:18 +0500

    work left
    score sharing
    players img , team img
    comment and news

---
+++ app/src/main/java/com/example/umairfyp/News_Adapter.java
@@ -0,0 +1,25 @@
+package com.example.umairfyp;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class News_Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
